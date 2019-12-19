1170442513761fd455c7aee8c8c5143827e5ccf2
liuyiyou <liuyiyou@ibalife.com>
2019-12-19 14:15:18 +0800

    增加rocketMQ实例

--- src/main/java/cn/liuyiyou/mq/kafka/MyKafkaProducer.java
+++ src/main/java/cn/liuyiyou/mq/kafka/MyKafkaProducer.java
@@ -15,11 +15,6 @@ public class MyKafkaProducer {
     public static Properties initConfig() {
         Properties props = new Properties();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-//        props.put(ProducerConfig.ACKS_CONFIG, "all");
-//        props.put(ProducerConfig.RETRIES_CONFIG, 0);
-//        props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
-//        props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
-//        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.CLIENT_ID_CONFIG, "myclient.id");
---
+++ src/main/java/cn/liuyiyou/mq/rocketmq/transaction/TransactionConsumer.java
@@ -0,0 +1,33 @@
+package cn.liuyiyou.mq.rocketmq.transaction;
+
+import cn.liuyiyou.mq.rocketmq.RocketMQConfig;
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
+/**
+ * @author: liuyiyou.cn
+ * @date: 2019/12/19
+ * @version: V1.0
+ */
+public class TransactionConsumer {
+
+    public static void main(String[] args) throws MQClientException {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer();
+        consumer.setConsumerGroup("transaction");
+        consumer.setNamesrvAddr(RocketMQConfig.NAME_ADDR);
+        consumer.subscribe("topicTest-t", "TagA || TagB || TagC || TagD || TagE");
+        consumer.registerMessageListener((MessageListenerConcurrently) (msgs, context) -> {
+            System.out.printf(Thread.currentThread().getName() + " consumerB Receive New Messages: " + msgs + "%n");
+            System.out.printf( "consumerB Receive New Messages: body " + new String(msgs.get(0).getBody()) + "%n");
+            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+        });
+        consumer.start();
+        System.out.printf("Broadcast ConsumerB Started.%n");
+    }
+}
